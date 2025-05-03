8fe8e53ea5b87199728298fda0792d58f94b5e37
GregSharov <grigorysharov@icloud.com>
2025-05-03 23:56:35 +0300

    changes in model schemes

--- Server/models/lessonScheme.js
+++ Server/models/lessonScheme.js
@@ -2,12 +2,10 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const lessonSchema = new Schema({
-    subject: { type: String, themes: [{ type: String, required: true }], required: true },
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    comments: [{ body: String, date: Date }],
-    date: { type: Date, default: Date.now },
-    rating: Number
+    subject: {
+        type: String,
+        themes: [{ type: String, required: true }], required: true
+    }
 });
 
 
