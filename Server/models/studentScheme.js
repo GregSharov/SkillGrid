efaef843c2e6968cad36c27fd8221eef0df92871
GregSharov <grigorysharov@icloud.com>
2025-05-04 00:16:15 +0300

    changes in model schemes

--- Server/models/studentScheme.js
+++ Server/models/studentScheme.js
@@ -2,6 +2,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const studentSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    sex: {
+        type: String,
+        required: true
+    },
     subject: {
         type: String,
         required: true,
