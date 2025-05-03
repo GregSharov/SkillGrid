a13f2bd72442e5ef9b4a6c5343371d8aa54aa624
GregSharov <grigorysharov@icloud.com>
2025-05-04 00:05:05 +0300

    changes in model schemes

--- Server/models/lessonScheme.js
+++ Server/models/lessonScheme.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 const lessonSchema = new Schema({
     subject: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     themes: [
         {
