4c9d743aeeab429d606d35923851ae299d8c1a90
GregSharov <grigorysharov@icloud.com>
2025-05-03 23:52:05 +0300

    changes in commentSchema.js

--- Server/models/lessonScheme.js
+++ Server/models/lessonScheme.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const lessonSchema = new Schema({
+    subject: { type: String },
     title: { type: String, required: true },
     author: { type: String, required: true },
     comments: [{ body: String, date: Date }],
