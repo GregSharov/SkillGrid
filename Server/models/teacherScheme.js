14a423b68f3a8c041ea2272acf8e0b357cd35491
GregSharov <grigorysharov@icloud.com>
2025-05-04 00:29:27 +0300

    changes in model schemes

---
+++ Server/models/teacherScheme.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const teacherSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+        trim: true,
+    },
+    sex: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    subjects: [
+        {
+            name: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            themes: [
+                {
+                    type: String,
+                    required: true
+                }
+            ]
+        }
+    ]
+});
+
+
+module.exports = teacherSchema;
