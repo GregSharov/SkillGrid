0b4c48649cc15847f55d4b0cae5e75fa6ab5eeb0
GregSharov <grigorysharov@icloud.com>
2025-05-07 19:48:48 +0300

    changes in model schemes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import './App.css';
-import AddNewUser from "./addNewUser.js";
+import addNewUser from "./addNewUser.js";
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <AddNewUser />
+      <addNewUser />
     </div>
   );
 }
