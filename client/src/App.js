bd92f1e4ebf3208d1787bb937ca01da7ec8d7725
GregSharov <grigorysharov@icloud.com>
2025-05-16 11:06:21 +0300

    write app.get request to show Student data on server side and showData.js frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 // import { useEffect, useState } from "react";
 import './App.css';
 import AddNewUser from "./addNewUser.js";
+import ShowData from "./showData.js";
 
 function App() {
   return (
     <div>
       <AddNewUser />
+      <ShowData />
     </div>
   );
 }
--- client/src/showData.js
+++ client/src/showData.js
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Show data to main page
 function ShowData() {
-    const [userData, setUserData] = useState(
-        {
+    const [userData, setUserData] = useState([]);
+    const url = 'http://localhost:3000/';
 
-        }
+    function fetchData() {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                const sortedData = data.flatMap(item => item.firstNames.map(firstName => ({
+                    student: item.firstName
+                }))).sort((a, b) => a.firstName - b.firstName);
+
+                setUserData(sortedData);
+            })
+            .catch(err => console.log('Error fetching data', err));
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            {userData}
+        </div>
     )
 }
+
+export default ShowData;
