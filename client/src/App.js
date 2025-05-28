c8ce8e8f961ef838eaadb2a41c209b1490a1c0af
GregSharov <grigorysharov@icloud.com>
2025-05-28 23:28:36 +0300

    clear App.js from unwanted imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 // import { useEffect, useState } from "react";
 import "./App.css";
-import AddNewUser from "./addNewUser.js";
-
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import SignIn from ".//signIn.js";
-import DisplayStudentsData from "./displayStudentsData.js";
-import DisplayTeacherData from "./displayTeachersData.js";
-import DisplaySubjectData from "./displaySubjectsData.js";
-// import LessonCard from "./lessonCard.js";
-
+// Importing components for routing
 import Layout from "./pages/Layout.js";
 import NoPage from "./pages/NoPage.js";
 import Home from "./pages/HomePage.js";
@@ -32,16 +24,7 @@ function App() {
       </Routes>
     </BrowserRouter>
   );
-  // return (
-  //   // <div>
-  //   //   <AddNewUser />
-  //   //   <SignIn />
-  //   //   <LessonCard />
-  //   //   <DisplayStudentsData />
-  //   //   <DisplayTeachersData />
-  //   //   <DisplaySubjectData />
-  //   // </div>
-  // );
+
 }
 
 export default App;
