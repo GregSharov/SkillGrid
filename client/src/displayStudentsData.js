3386ae3be4804c39428caa0826fefcc300dd429f
GregSharov <grigorysharov@icloud.com>
2025-06-04 17:24:02 +0300

    clean code from useless lines of code

--- client/src/displayStudentsData.js
+++ client/src/displayStudentsData.js
@@ -4,7 +4,6 @@ import fetchData from "./services/fetchData";
 // Show data to main page
 function ShowStudentData() {
   const [studentData, setStudentData] = useState([]);
-  const url = "http://localhost:3000/students";
 
   useEffect(() => {
     fetchData("students")
@@ -23,7 +22,11 @@ function ShowStudentData() {
             {item.firstName} {item.lastName}
           </p>
           {/* Turn ISO date 2021-11-18T00:00:00.000Z to usual format dd-mm-yyyy */}
-          <p>{new Date (item.dateOfBirth).toLocaleDateString('en-GB').replaceAll("/", "-")}</p>
+          <p>
+            {new Date(item.dateOfBirth)
+              .toLocaleDateString("en-GB")
+              .replaceAll("/", "-")}
+          </p>
           <p>{item.email}</p>
           <p>{item.phone}</p>
           {/* <p>{item.password}</p> */}
