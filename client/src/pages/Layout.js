7d728c28d88c95b77b40d11b01d42546d85749a4
ivan latisov <ivanlatisov@MacBook-Air-yrnh.local>
2025-06-04 18:24:24 +0300

    add a imagess

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
        <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src={logo} class="h-10" alt="SkillGrid Logo" />
+        <img src={logo} class="h-9" alt="SkillGrid Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
     </a>
     <div class="flex md:order-2">
