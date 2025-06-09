92410317dfa35fbcc14c35a05b7a6f241f74f280
GregSharov <grigorysharov@icloud.com>
2025-06-09 23:15:02 +0300

    add filter data by id function

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,13 +1,22 @@
-function fetchData(model) {
+function fetchData(model, filterById) {
   const url = "http://localhost:3000/";
+
+  function filterData(data, filterById = null) {
+    if (filterById === null) {
+      return data;
+    }
+    return data.filter((item) => Object.values(item).includes(filterById));
+  }
 
   return fetch(`${url}${model}`)
     .then((res) => res.json())
     .then((data) => {
+      const filteredData = filterData(data, filterById);
+
       if (model !== "subjects") {
-        return data.sort((a, b) => a.firstName.localeCompare(b.firstName));
+        return filteredData.sort((a, b) => a.firstName.localeCompare(b.firstName));
       } else if (model === "subjects") {
-        return data.sort((a, b) => a.name.localeCompare(b.name));
+        return filteredData.sort((a, b) => a.name.localeCompare(b.name));
       }
     })
     .catch((error) => {
