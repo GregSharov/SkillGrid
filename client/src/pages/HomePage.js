47f057ce5367b628fb6968d730ced79657c45c90
GregSharov <grigorysharov@icloud.com>
2025-06-09 23:22:06 +0300

    fix DisplaySubjectsData and DisplayLessonsData modules to receive/send properties

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import DisplaySubjectData from "../displaySubjectsData.js";
 import DisplayLessonData from "../displayLessonsDada.js";
 import DisplayTeacherData from "../displayTeachersData.js";
 
 const Home = () => {
+  const [selectedSubjectId, setSelectedSubjectId] = useState(null);
   return (
     <div>
-      <DisplaySubjectData />
-      <DisplayLessonData />
+      {/* <DisplaySubjectData /> */}
+      <DisplaySubjectData onSubjectClick={setSelectedSubjectId} />
+      {/* <DisplayLessonData /> */}
+      <DisplayLessonData id={selectedSubjectId} />
       <DisplayTeacherData />
     </div>
   );
