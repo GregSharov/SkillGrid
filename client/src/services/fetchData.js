6aa0fc5b2b90f27f35cfe22c91ad290e39273afa
GregSharov <grigorysharov@icloud.com>
2025-06-16 08:09:03 +0300

    improve search engine to search items in nested databases

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -1,11 +1,29 @@
 function fetchData(model, filterById) {
   const url = "http://localhost:3000/";
+  console.log("CHECK: ", filterById);
+
+  function deepSearch(obj, searchTerm) {
+    const term = searchTerm.toLowerCase();
+
+    function recurse(value) {
+      if (typeof value === "string") {
+        return value.toLowerCase().includes(term);
+      } else if (typeof value === "number") {
+        return value.toString().includes(term);
+      } else if (Array.isArray(value)) {
+        return value.some(recurse);
+      } else if (typeof value === "object" && value !== null) {
+        return Object.values(value).some(recurse);
+      }
+      return false;
+    }
+
+    return recurse(obj);
+  }
 
   function filterData(data, filterById = null) {
-    if (filterById === null) {
-      return data;
-    }
-    return data.filter((item) => Object.values(item).includes(filterById));
+    if (!filterById) return data;
+    return data.filter((item) => deepSearch(item, filterById));
   }
 
   return fetch(`${url}${model}`)
@@ -14,9 +32,11 @@ function fetchData(model, filterById) {
       const filteredData = filterData(data, filterById);
 
       if (model !== "subjects") {
-        return filteredData.sort((a, b) => a.firstName.localeCompare(b.firstName));
-      } else if (model === "subjects") {
-        return filteredData.sort((a, b) => a.name.localeCompare(b.name));
+        return filteredData.sort((a, b) =>
+          a.firstName?.localeCompare(b.firstName)
+        );
+      } else {
+        return filteredData.sort((a, b) => a.name?.localeCompare(b.name));
       }
     })
     .catch((error) => {
@@ -26,3 +46,33 @@ function fetchData(model, filterById) {
 }
 
 export default fetchData;
+
+// function fetchData(model, filterById) {
+//   const url = "http://localhost:3000/";
+//   console.log("CHECK: ", filterById);
+
+//   function filterData(data, filterById = null) {
+//     if (filterById === null) {
+//       return data;
+//     }
+//     return data.filter((item) => Object.values(item).includes(filterById));
+//   }
+
+//   return fetch(`${url}${model}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       const filteredData = filterData(data, filterById);
+
+//       if (model !== "subjects") {
+//         return filteredData.sort((a, b) => a.firstName.localeCompare(b.firstName));
+//       } else if (model === "subjects") {
+//         return filteredData.sort((a, b) => a.name.localeCompare(b.name));
+//       }
+//     })
+//     .catch((error) => {
+//       console.error("Error fetching data:", error);
+//       throw error;
+//     });
+// }
+
+// export default fetchData;
