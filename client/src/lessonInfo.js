316f8e25534551c5f0009244025b03c5dd91b497
GregSharov <grigorysharov@icloud.com>
2025-06-19 05:49:20 +0300

    fix bugs to show data in lesson card info

--- client/src/lessonInfo.js
+++ client/src/lessonInfo.js
@@ -1,32 +1,55 @@
 import {  useState, useEffect } from "react";
 import fetchData from "./services/fetchData";
 import filterData from "./services/filterData";
+import TeacherCard from "./components/TeacherCard";
 
 
 const DisplayLessonsInfo = ({ lessonId }) => {
   const [lessonData, setLessonData] = useState([]);
+  const [teachersData, setTeachersData] = useState([]);
   console.log("Lesson ID:", lessonId);
 
   useEffect(() => {
     fetchData("subjects")
       .then((data) => {
-        const filtered = filterData(data); // your logic here
+        console.log("Fetched subjects data:", data);
+        const filtered = filterData(data, lessonId); // returns Object of Subject contains needed lessonId
+        console.log("Filtered subjects data:", filtered);
         const allLessons = filtered.flatMap(item => item.lessons || []);
         const singleLesson = allLessons.find(lesson => lesson._id === lessonId);
+        console.log("Single lesson data:", singleLesson);
         setLessonData(singleLesson);
       })
       .catch((err) => console.error("Error fetching lesson:", err));
   }, [lessonId]);
+
+  useEffect(() => {
+    fetchData("teachers")
+      .then((data) => {
+        console.log("Fetched teachers data:", data);
+        const filteredTeachers = filterData(data, lessonData.name);
+        console.log("Filtered teachers data:", filteredTeachers);
+        setTeachersData(filteredTeachers);
+      })
+      .catch((err) => console.error("Error fetching teachers:", err));
+  }, [lessonData.name]);
 
   if (!lessonData) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div className="lesson-info">
+    <div>
+      <div className="lesson-info">
       <h1>{lessonData.name}</h1>
       <img src={lessonData.image} alt={lessonData.name} />
       <p>{lessonData.description}</p>
+    </div>
+    <div>
+      {teachersData.map((teacher, index) => (
+        <TeacherCard key={index} teacher={teacher} />
+      ))}
+    </div>
     </div>
   );
 };
