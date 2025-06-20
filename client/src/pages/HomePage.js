df206be6a5deec5fe3af4ef63a718d0f291c34f3
GregSharov <grigorysharov@icloud.com>
2025-06-20 23:46:17 +0300

    remove unnessesary comments

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import DisplaySubjectData from "../displaySubjectsData.js";
 import DisplayLessonData from "../displayLessonsDada.js";
-import DisplayTeacherData from "../displayTeachersData.js";
 import LessonInfoPage from "../pages/lessonInfoPage.js";
 
 const Home = () => {
@@ -9,15 +8,12 @@ const Home = () => {
   const [selectedLessonId, setSelectedLessonId] = useState(null);
   return (
     <div>
-      {/* <DisplaySubjectData /> */}
       <DisplaySubjectData onSubjectClick={setSelectedSubjectId} />
-      {/* <DisplayLessonData /> */}
       <DisplayLessonData
         id={selectedSubjectId}
         lessonInfo={setSelectedLessonId}
       />
       <LessonInfoPage id={selectedLessonId} />
-      <DisplayTeacherData />
     </div>
   );
 };
