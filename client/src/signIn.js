31092f87dc8c7767d33559bad9d69fda10b3b9b1
GregSharov <grigorysharov@icloud.com>
2025-06-22 07:28:51 +0300

    add isTeacher property to pass to openAccountPage

--- client/src/signIn.js
+++ client/src/signIn.js
@@ -29,6 +29,7 @@ function SignIn() {
 
     const data = await res.json();
     const userId = data.user.id;
+    const isTeacher = data.user.isTeacher;
     alert(`Server says: ${data.message}`);
     if (data.message === "Invalid email or password.") {
       // Reset the form
@@ -38,14 +39,14 @@ function SignIn() {
         isTeacher: false,
       });
     } else {
-      openAcountPage(userId);
+      openAccountPage(userId, isTeacher);
     }
   };
 
   // This function is used to navigate to the account page after successful login
   const navigate = useNavigate();
-  const openAcountPage = (userId) => {
-    navigate("/account", { state: { userId } });
+  const openAccountPage = (userId, isTeacher) => {
+    navigate("/account", { state: { userId, isTeacher } });
   };
 
   // This function is used to render the form
@@ -80,7 +81,7 @@ function SignIn() {
           </div>
           <div className="">
             <label
-              for="isTeacher"
+              htmlFor="isTeacher"
               className="flex items-center w-full transform bg-transparent text-lg duration-300 focus-within:border-indigo-500"
             >
               <input
@@ -98,17 +99,6 @@ function SignIn() {
           <button
             type="submit"
             className="transform rounded-sm bg-indigo-600 py-2 font-bold duration-300 hover:bg-indigo-400"
-            // login to account page
-            onClick={() => {
-              // console.log("Form Data:", formData);
-              // if (formData.email && formData.password) {
-              //   // Navigate to account page after successful login
-              //   openAcountPage()
-              //   alert("Login successful! Redirecting to account page...");
-              // } else {
-              //   alert("Invalid Email or Passwords. Please try again!");
-              // }
-            }}
           >
             LOG IN
           </button>
