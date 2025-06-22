e01cb1177ff2781b756fb02ef4c3ea3d60d4c775
GregSharov <grigorysharov@icloud.com>
2025-06-22 07:08:43 +0300

    fix the same id issue in search input field

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -57,7 +57,7 @@ const Layout = () => {
               </div>
               <input
                 type="text"
-                id="search-navbar"
+                id="search-navbar-desktop"
                 className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
               />
@@ -111,7 +111,7 @@ const Layout = () => {
               </div>
               <input
                 type="text"
-                id="search-navbar"
+                id="search-navbar-mobile"
                 className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
               />
